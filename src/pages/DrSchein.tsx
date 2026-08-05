d8d0942c616f281144924e38277af3e49b04798e
Amir Paluch <amir.paluch@gmail.com>
2026-08-05 14:46:40 +0300

    fix drschein url typo3

--- src/pages/DrSchein.tsx
+++ src/pages/DrSchein.tsx
@@ -2,7 +2,7 @@ export default function DrSchein() {
   return (
     <div style={{ minHeight: '100vh', background: '#000000', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
       <iframe
-        src="https://schein.rapid-image.net/CP/"
+        src="https://schein.rapid-image.net/CP/login"
         width="500"
         height="600"
         title="פורטל לקוחות"
